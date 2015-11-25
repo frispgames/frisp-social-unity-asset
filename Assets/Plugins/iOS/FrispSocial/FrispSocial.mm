d20a427c69b100afff6b06718e3aee9db1312b02
Michael Treacher <m.w.treacher@gmail.com>
2015-11-25 19:12:07 +1100

    Support iPad for iOS8 +

--- Assets/Plugins/iOS/FrispSocial/FrispSocial.mm
+++ Assets/Plugins/iOS/FrispSocial/FrispSocial.mm
@@ -16,10 +16,16 @@
   UIImage *image = [[UIImage alloc] initWithData:imageData];
 
   socialViewController = [[UIActivityViewController alloc] initWithActivityItems:@[text, image] applicationActivities:nil];
-
+  
   UIViewController *rootViewController =  UnityGetGLViewController();
-
-  [rootViewController presentViewController:socialViewController animated:YES completion:nil];
+  
+  if ([socialViewController respondsToSelector:@selector(popoverPresentationController)]) {
+    // IOS8 iPad
+    UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:socialViewController];
+    [popup presentPopoverFromRect:CGRectMake(rootViewController.view.frame.size.width/2, rootViewController.view.frame.size.height/4, 0, 0)inView:rootViewController.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+  } else {
+        [rootViewController presentViewController:socialViewController animated:YES completion:nil];
+  }
 }
 
 extern "C" { 
